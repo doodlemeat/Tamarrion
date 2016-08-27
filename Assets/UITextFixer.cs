a2f26ee4fb25b178f133ef3fef3ca877245aedc6
Anthon Fredriksson <anthonnorrby@gmail.com>
2016-08-28 00:01:24 +0200

    lågupplöst text i ingame-menyn fixad.

---
+++ Assets/UITextFixer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+
+namespace Tamarrion {
+	public class UITextFixer : MyMonoBehaviour {
+		void Awake () {
+			AddListener<EnableHUDEvent> (OnEnableHud);
+		}
+
+		void OnDestroy() {
+			RemoveListener<EnableHUDEvent> (OnEnableHud);
+		}
+		
+		void OnEnableHud(EnableHUDEvent e) {
+			Text[] texts = GetComponentsInChildren<Text> ();
+			foreach ( Text text in texts ) {
+				text.SetAllDirty ();
+			}
+		}
+	}
+}
