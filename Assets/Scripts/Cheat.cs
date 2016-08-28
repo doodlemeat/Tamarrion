d47b8c8d29a71c27ee5743783ed4eec64bd0a18b
Anthon Fredriksson <anthonnorrby@gmail.com>
2016-08-28 13:05:15 +0200

    Valac tomb dörren "exploderas" ut fixen

---
+++ Assets/Scripts/Cheat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// Cheat system enables you to teleport to boss locations
+// Only works when playing from editor
+// Extend this class further when neccessary 
+
+namespace Tamarrion {
+	public class Cheat : MyMonoBehaviour {
+		[SerializeField]
+		string valacTeleportKey = "F1";
+
+		[SerializeField]
+		Transform valacTeleportPosition;
+
+		void Update() {
+			if(Input.GetKeyDown(KeyCode.F1)) {
+				Player.player.transform.position = valacTeleportPosition.position;
+			}
+		}
+	}
+}
