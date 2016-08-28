d7af92baf520ef7f1c29c4ffb3aac2aaf0ccff93
Anthon Fredriksson <anthonnorrby@gmail.com>
2016-08-28 21:11:16 +0200

    Extended the cheat system. check readme.

--- Assets/Scripts/Cheat.cs
+++ Assets/Scripts/Cheat.cs
@@ -7,25 +7,45 @@
 namespace Tamarrion {
 	public class Cheat : MyMonoBehaviour {
 		[SerializeField]
-		string valacTeleportKey = "F1";
+		string valacTeleportKey = "f1";
 
 		[SerializeField]
-		string nithTeleportKey = "F2";
+		string valacTeleportKeyBeforeActivate = "f2";
+
+		[SerializeField]
+		string nithTeleportKey = "f3";
+
+		[SerializeField]
+		string nithTeleportKeyBeforeActive = "f4";
 
 		[SerializeField]
 		Transform valacTeleportPosition;
 
 		[SerializeField]
+		Transform valacTeleportPositionBeforeActivate;
+
+		[SerializeField]
 		Transform nithTeleportPosition;
+
+		[SerializeField]
+		Transform nithTeleportPositionBeforeActive;
 
 		void Update() {
 			if ( Application.isEditor ) {
-				if ( Input.GetKeyDown (valacTeleportKey) ) {
+				if ( Input.GetKeyDown (valacTeleportKey.ToLower()) ) {
 					Player.player.transform.position = valacTeleportPosition.position;
 				}
 
-				if ( Input.GetKeyDown (nithTeleportKey) ) {
+				if ( Input.GetKeyDown (nithTeleportKey.ToLower()) ) {
 					Player.player.transform.position = nithTeleportPosition.position;
+				}
+
+				if ( Input.GetKeyDown (valacTeleportKeyBeforeActivate.ToLower ()) ) {
+					Player.player.transform.position = valacTeleportPositionBeforeActivate.position;
+				}
+
+				if ( Input.GetKeyDown (nithTeleportKeyBeforeActive.ToLower ()) ) {
+					Player.player.transform.position = nithTeleportPositionBeforeActive.position;
 				}
 			}
 		}
