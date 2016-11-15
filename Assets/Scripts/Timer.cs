812da672f7191ecaddb663b5e0da0cc51d0a0d95
Julian Thijssen <Nimthora@gmail.com>
2016-11-15 16:18:54 +0100

    Added Timer script to replace TopgunTimer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tamarrion {
+    public class Timer {
+        private float time;
+        private float duration;
+
+        private bool finished;
+
+        public void Update() {
+            if (finished)
+                return;
+
+            time += Time.deltaTime;
+
+            if (time > duration) {
+                time = duration;
+                finished = true;
+            }
+        }
+
+        public void Start(float duration) {
+            this.duration = duration;
+            finished = false;
+        }
+
+        public float Progress() {
+            return time / duration;
+        }
+
+        public bool isFinished() {
+            return finished;
+        }
+    }
+}
