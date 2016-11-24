6078c71a85cb7ab215311f744be8c74256079297
Julian Thijssen <Nimthora@gmail.com>
2016-11-24 05:39:33 +0100

    Moved DifficultyManager Awake() logic to OnAwake()

--- Assets/Scripts/Managers/DifficultyManager.cs
+++ Assets/Scripts/Managers/DifficultyManager.cs
@@ -4,50 +4,50 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Tamarrion {
-	[UnitySingleton (false, true)]
-	public class DifficultyManager : UnitySingleton<DifficultyManager> {
-		public static Difficulty current;
+    [UnitySingleton(false, true)]
+    public class DifficultyManager : UnitySingleton<DifficultyManager> {
+        public static Difficulty current;
 
-		[System.Serializable]
-		class DifficultyStruct {
-			public Difficulty diff = Difficulty.Normal;
-		}
+        [System.Serializable]
+        class DifficultyStruct {
+            public Difficulty diff = Difficulty.Normal;
+        }
 
-		void Awake() {
-			current = Difficulty.Normal;
-			Load();
-		}
+        protected override void OnAwake() {
+            current = Difficulty.Normal;
+            Load();
+        }
 
-		void OnDisable() {
-			Save();
-		}
+        void OnDisable() {
+            Save();
+        }
 
-		void Save() {
-			//Debug.Log("saving difficulty: " + Current_difficulty);
+        void Save() {
+            //Debug.Log("saving difficulty: " + Current_difficulty);
 
-			BinaryFormatter bff = new BinaryFormatter();
-			FileStream ffs = File.Create(Application.persistentDataPath + "/difficulty.dat");
+            BinaryFormatter bff = new BinaryFormatter();
+            FileStream ffs = File.Create(Application.persistentDataPath + "/difficulty.dat");
 
-			DifficultyStruct invTest = new DifficultyStruct();
-			invTest.diff = current;
+            DifficultyStruct invTest = new DifficultyStruct();
+            invTest.diff = current;
 
-			bff.Serialize(ffs, invTest);
-			ffs.Close();
-		}
+            bff.Serialize(ffs, invTest);
+            ffs.Close();
+        }
 
-		void Load() {
-			if (!File.Exists(Application.persistentDataPath + "/difficulty.dat"))
-				return;
+        void Load() {
+            if (!File.Exists(Application.persistentDataPath + "/difficulty.dat"))
+                return;
 
-			BinaryFormatter bff = new BinaryFormatter();
-			FileStream ffs = File.Open(Application.persistentDataPath + "/difficulty.dat", FileMode.Open);
+            BinaryFormatter bff = new BinaryFormatter();
+            FileStream ffs = File.Open(Application.persistentDataPath + "/difficulty.dat", FileMode.Open);
 
-			DifficultyStruct invTest = (DifficultyStruct)bff.Deserialize(ffs);
-			ffs.Close();
+            DifficultyStruct invTest = (DifficultyStruct)bff.Deserialize(ffs);
+            ffs.Close();
 
-			current = invTest.diff;
+            current = invTest.diff;
 
-			//Debug.Log("loaded difficulty: " + Current_difficulty);
-		}
-	}
+            //Debug.Log("loaded difficulty: " + Current_difficulty);
+        }
+    }
 }
